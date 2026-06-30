5ddd25879f7ec33894e72d9c89559cbbfefced71
m-sef <seth.l.moore0506@gmail.com>
2026-06-30 12:48:18 -0400

    Add StrictHostKeyChecking=no to SSH commands to avoid host key prompts on new nodes

--- scripts/configure_all_nodes.sh
+++ scripts/configure_all_nodes.sh
@@ -3,9 +3,9 @@
 NODE_COUNT=$1
 
 for ((i=1; i<=NODE_COUNT; i++)); do
-    ssh 10.10.1.$i "sudo /local/repository/scripts/disable_hyperthreading.sh"
-    ssh 10.10.1.$i "sudo /local/repository/scripts/disable_turboboost.sh"
-    ssh 10.10.1.$i "sudo /local/repository/scripts/set_all_cores_policy.sh performance"
+    ssh -o StrictHostKeyChecking=no 10.10.1.$i "sudo /local/repository/scripts/disable_hyperthreading.sh"
+    ssh -o StrictHostKeyChecking=no 10.10.1.$i "sudo /local/repository/scripts/disable_turboboost.sh"
+    ssh -o StrictHostKeyChecking=no 10.10.1.$i "sudo /local/repository/scripts/set_all_cores_policy.sh performance"
 done
 
 exit 0
