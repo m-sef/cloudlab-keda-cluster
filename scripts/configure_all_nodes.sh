114a93b6d3e05b1a857e221435a0361a63c099e7
m-sef <seth.l.moore0506@gmail.com>
2026-06-30 12:49:52 -0400

    Skip configuring the local node by checking if its IP matches before SSHing

--- scripts/configure_all_nodes.sh
+++ scripts/configure_all_nodes.sh
@@ -3,9 +3,15 @@
 NODE_COUNT=$1
 
 for ((i=1; i<=NODE_COUNT; i++)); do
-    ssh -o StrictHostKeyChecking=no 10.10.1.$i "sudo /local/repository/scripts/disable_hyperthreading.sh"
-    ssh -o StrictHostKeyChecking=no 10.10.1.$i "sudo /local/repository/scripts/disable_turboboost.sh"
-    ssh -o StrictHostKeyChecking=no 10.10.1.$i "sudo /local/repository/scripts/set_all_cores_policy.sh performance"
+    NODE=10.10.1.$i
+
+    if ip -4 addr show | grep -q "inet ${NODE}/"; then
+        continue
+    fi
+
+    ssh -o StrictHostKeyChecking=no $NODE "sudo /local/repository/scripts/disable_hyperthreading.sh"
+    ssh -o StrictHostKeyChecking=no $NODE "sudo /local/repository/scripts/disable_turboboost.sh"
+    ssh -o StrictHostKeyChecking=no $NODE "sudo /local/repository/scripts/set_all_cores_policy.sh performance"
 done
 
 exit 0
