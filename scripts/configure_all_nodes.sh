c791923d26391932f00b75ce544c15d3b76d880a
m-sef <seth.l.moore0506@gmail.com>
2026-06-30 16:39:27 -0400

    Configure all nodes

--- scripts/configure_all_nodes.sh
+++ scripts/configure_all_nodes.sh
@@ -7,11 +7,15 @@ if ! "$SCRIPT_DIR/is_master.sh"; then
     exit 1
 fi
 
+sudo /local/repository/scripts/disable_hyperthreading.sh
+sudo /local/repository/scripts/disable_turboboost.sh
+sudo /local/repository/scripts/set_all_cores_policy.sh performance
+
 for ((i=1; i<=NODE_COUNT; i++)); do
     NODE=10.10.1.$i
 
     if ! ssh -o StrictHostKeyChecking=no $NODE "sudo /local/repository/scripts/disable_hyperthreading.sh"; then
-        echo "Failed to SSH into $NODE, skipping..."
+        echo "Failed to SSH '$NODE', skipping..."
         continue
     fi
     ssh -o StrictHostKeyChecking=no $NODE "sudo /local/repository/scripts/disable_turboboost.sh"
